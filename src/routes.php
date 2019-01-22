24fda2ce87bf3cf4648cd62bc937a8129fa04b4d
maulanardy <maulanardy.dev@gmail.com>
2019-01-22 20:01:52 +0700

    tes

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,51 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->post('/login', function (Request $request, Response $response, array $args) {
+ 
+    $input = $request->getParsedBody();
+    // $sql = "SELECT * FROM users WHERE email= :email";
+    // $sth = $this->db->prepare($sql);
+    // $sth->bindParam("email", $input['email']);
+    // $sth->execute();
+    // $user = $sth->fetchObject();
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    if($input['email'] == 'arjunphp@gmail.com'){
+        $user = new stdClass();
+        $user->password = "$2y$10$2N74YBkxYXPtEtFTxynuxeEn9OH9BZ.wI4ldZr00n1FX5q09/llbO";
+    }
+ 
+    // verify email address.
+    if(!$user) {
+        return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);  
+    }
+ 
+    // verify password.
+    if (!password_verify($input['password'],$user->password)) {
+        return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);  
+    }
+ 
+    $settings = $this->get('settings'); // get settings array.
+    
+    $token = JWT::encode(['id' => $user->id, 'email' => $user->email], $settings['jwt']['secret'], "HS256");
+ 
+    return $this->response->withJson(['token' => $token]);
+ 
+});
+
+$app->group('/api', function(\Slim\App $app) {
+ 
+    $app->get('/user',function(Request $request, Response $response, array $args) {
+        print_r($request->getAttribute('decoded_token_data'));
+ 
+        /*output 
+        stdClass Object
+            (
+                [id] => 2
+                [email] => arjunphp@gmail.com
+            )
+                    
+        */
+    });
+   
 });
--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,12 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+        "db" => [            
+            "host" => "localhost",             
+            "dbname" => "slim",             
+            "user" => "root",            
+            "pass" => ""        
+        ],
+        'jwt' => 'sekedakerrratnaputputkatkatkatsukidupatpatnaputputcatcatcat'
     ],
 ];
